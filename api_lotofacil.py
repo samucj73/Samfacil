598da28009455c9da62babf7899446c06a720060
samucj73 <samu.rcj@gmail.com>
2025-05-26 09:46:28 -0300

    Update api_lotofacil.py

--- api_lotofacil.py
+++ api_lotofacil.py
@@ -1,6 +1,6 @@
 import requests
 
-def capturar_ultimos_resultados(qtd=300):
+def capturar_ultimos_resultados(qtd=2500):
     url_base = "https://loteriascaixa-api.herokuapp.com/api/lotofacil/"
     concursos = []
 
